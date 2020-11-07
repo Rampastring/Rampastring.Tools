77a8b22feceb5233d47c6c69892e6bf80c81dd63
Rampastring <rampastring@outlook.com>
2020-11-07 12:33:38 +0200

    Use auto-incrementing version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rampastring")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Rami 'Rampastring' Pasanen 2016")]
+[assembly: AssemblyCopyright("Copyright © Rami 'Rampastring' Pasanen 2016-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
