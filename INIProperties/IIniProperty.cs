4eeeed80291a2e3e37f047f2586a673db6546925
Rans4ckeR <ransacker@outlook.com>
2022-04-18 04:09:19 +0200

    .NET6

--- INIProperties/IIniProperty.cs
+++ INIProperties/IIniProperty.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Rampastring.Tools.INIProperties
+﻿namespace Rampastring.Tools.INIProperties
 {
     /// <summary>
     /// An interface for INI properties.
--- INIProperties/IntProperty.cs
+++ INIProperties/IntProperty.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Rampastring.Tools.INIProperties
+﻿namespace Rampastring.Tools.INIProperties
 {
     /// <summary>
     /// An integer to be parsed from an INI file.
